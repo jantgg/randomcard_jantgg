c9e43625806cc023cfb40921f3a1a2fec04e1a5d
jantgg <smosh360@hotmail.com>
2022-12-05 12:41:42 +0000

    card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let icons = ["♦", "♥", "♠", "♣"];
+
+  let cardNumber = document.querySelector("#cardNumber");
+  cardNumber.style.fontSize = "200px";
+  let cardIcon = document.querySelectorAll("#cardIcon");
+
+  cardNumber.innerHTML = numbers[Math.floor(Math.random() * numbers.length)];
+
+  let randomIconIndex = Math.floor(Math.random() * icons.length);
+
+  for (let x = 0; x < cardIcon.length; x++) {
+    if (randomIconIndex <= 1) {
+      cardIcon[x].style.color = "red";
+    }
+    cardIcon[x].innerHTML = icons[randomIconIndex];
+    cardIcon[x].style.fontSize = "100px";
+  }
 };
